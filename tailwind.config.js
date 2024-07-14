0e5498f02e6a0311ef84cc97410dd08656ee9617
Al-Amin Biswas <abjessbd@gmail.com>
2024-07-14 17:27:12 +0600

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '1.5rem'
+      }
+    },
+    extend: {
+      colors: {
+        primaryColor: '#010A5E',
+        primaryColorLight: '#010D78',
+        yelloColor: '#FFCC00',
+        redColor: '#CC3433',
+        greenColor: '#007936',
+        blackColor: '#000000',
+        whiteColor: '#FFFFFF'
+      }
+    },
+  },
+  plugins: [],
+}
